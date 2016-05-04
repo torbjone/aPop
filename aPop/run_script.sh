5d5e052f0a1f42e368ce9899ddccff28d5261dfa
Torbjørn V Ness <torbness@gmail.com>
2016-05-04 16:03:33 +0200

    Updated run scripts

--- aPop/run_script.sh
+++ aPop/run_script.sh
@@ -6,16 +6,8 @@
 #PBS -m abe
 #PBS -A nn4661k
 
-#folder=generic_study
-#filename=generic_study.py
-
-#cd /home/$USER/work/aLFP/trunk/aLFP
-#mkdir /global/work/torbness/aLFP/$folder
-#mkdir /global/work/torbness/aLFP/$folder/hay
-#cp $filename /global/work/torbness/aLFP/$folder
 cd /global/work/torbness/aPop/aPop
-
-
+python Population.py initialize
 mpirun -np 32 python Population.py MPI
 
 python Population.py sum
