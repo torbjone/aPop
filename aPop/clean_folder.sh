b145d542c8416ce0d68ff15af30c86d3374ac295
Torbjørn V Ness <torbness@gmail.com>
2016-10-11 15:33:43 +0200

    Clean-time

--- aPop/clean_folder.sh
+++ aPop/clean_folder.sh
@@ -2,19 +2,19 @@
 #!/bin/bash
 
 #PBS -lnodes=1:ppn=1
-#PBS -lwalltime=2:00:00
+#PBS -lwalltime=1:00:00
 #PBS -lpmem=2000MB
 #PBS -m abe
 #PBS -A nn4661k
 
-cd /global/work/torbness/aPop/hbp_population/simulations
+cd /global/work/torbness/aPop/population/simulations
 
-rm *sig_*_0???0.npy
-rm *sig_*_0???2.npy
-rm *sig_*_0???3.npy
-rm *sig_*_0???4.npy
-rm *sig_*_0???5.npy
-rm *sig_*_0???6.npy
-rm *sig_*_0???7.npy
-rm *sig_*_0???8.npy
-rm *sig_*_0???9.npy
+rm *sig_*classic*_0???0.npy
+rm *sig_*classic*_0???2.npy
+rm *sig_*classic*_0???3.npy
+rm *sig_*classic*_0???4.npy
+rm *sig_*classic*_0???5.npy
+rm *sig_*classic*_0???6.npy
+rm *sig_*classic*_0???7.npy
+rm *sig_*classic*_0???8.npy
+rm *sig_*classic*_0???9.npy
