2084b65b2132ab34e16a4823b685b1a0dd90a750
Torbjørn V Ness <torbness@gmail.com>
2017-10-26 08:41:50 +0200

    Run to 10000 cells with generic population. Will skip population that are finished

--- aPop/run_script_slurm.sh
+++ aPop/run_script_slurm.sh
@@ -2,13 +2,13 @@
 ################################################################################
 #SBATCH --job-name Population_slurmtest
 #SBATCH --time 48:00:00
-#SBATCH -o population_output3.txt
-#SBATCH -e population_output_error3.txt
-#SBATCH --ntasks 160
+#SBATCH -o population_output6.txt
+#SBATCH -e population_output_error6.txt
+#SBATCH --ntasks 16
 #SBATCH --mem-per-cpu=4000MB
 #SBATCH --mail-type=ALL
 
 ################################################################################
 unset DISPLAY # slurm appear to create a problem with too many displays
-srun --mpi=pmi2 python Population.py MPI
+#srun --mpi=pmi2 python Population.py MPI
 mpirun -np $SLURM_NTASKS python Population.py MPI
