f076750517c085f79d7e82563e7f2e61fd499b4f
Torbjørn V Ness <torbness@gmail.com>
2017-11-16 12:05:31 +0100

    Removed lagacy code from

--- aPop/run_script.sh
+++ aPop/run_script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+################################################################################
+#SBATCH --job-name stick_population
+#SBATCH --time 48:00:00
+#SBATCH -o output_stick.txt
+#SBATCH -e output_error_stick.txt
+#SBATCH --ntasks 80
+#SBATCH --mem-per-cpu=4000MB
+#SBATCH --mail-type=ALL
 
-#PBS -lnodes=1:ppn=16
-#PBS -lwalltime=20:00:00
-#PBS -lpmem=2000MB
-#PBS -m abe
-#PBS -A nn4661k
-
-cd /global/work/torbness/aPop/aPop
-#python Population.py initialize
-mpirun -np 16 python Population.py MPI
-#mpirun -np 16 python Population.py sum
+################################################################################
+unset DISPLAY # slurm appear to create a problem with too many displays
+mpirun -np $SLURM_NTASKS python Population.py MPI
