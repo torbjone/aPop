793ee460632da748d222308699eb6850aa9e2b8e
Torbjørn V Ness <torbness@gmail.com>
2018-02-06 09:29:41 +0100

    Added posibility to introduce Ih plateau

---
+++ aPop/sum_signal.py
@@ -0,0 +1,37 @@
+
+
+
+sim_name = 'hay_classic'     # Control simulation
+
+from Population import sum_and_remove
+
+if sim_name == "stick_generic":
+    from param_dicts import stick_population_params as param_dict
+elif sim_name == "hay_generic":
+    from param_dicts import generic_population_params as param_dict
+elif sim_name == "hay_classic":
+    from param_dicts import classic_population_params as param_dict
+elif sim_name == "hbp_classic":
+    from param_dicts import hbp_population_params as param_dict
+elif sim_name == "generic_multimorph":
+    from param_dicts import multimorph_population_params as param_dict
+else:
+    raise RuntimeError("Unrecognized conductance")
+
+if "generic" in sim_name:
+    param_dict.update({'input_region': sys.argv[1],
+                   'correlation': float(sys.argv[2]),
+                   'distribution': "linear_increase",
+                   'conductance_type': 'generic',
+                   'mu': None if sys.argv[3] == "None" else float(sys.argv[3]),
+                   'holding_potential': -80.,
+                   'cell_number': 0})
+else:
+    param_dict.update({'input_region': sys.argv[1],
+                       'correlation': float(sys.argv[2]),
+                       'conductance_type': 'Ih_plateau',
+                       'holding_potential': -70.,
+                       'cell_number': 0})
+num_cells = int(sys.argv[-1])
+
+sum_and_remove(param_dict, num_cells)
